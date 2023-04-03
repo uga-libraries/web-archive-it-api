5f9e2f19b69650102211264d4fbb5a63bb745060
Adriane Hanson <ahansonuga@gmail.com>
2023-04-03 14:09:43 -0400

    Make generic csv functions
    Do report-specific calculations within that report's script and only use the shared function to save to a CSV

--- shared_functions.py
+++ shared_functions.py
@@ -6,22 +6,29 @@ import configuration as c
 
 
 def get_metadata_value(data, field):
-    """Looks up the value(s) of a field in the Archive-It API output for a particular collection or seed. If the
-    field is not in the output, returns the string NONE instead. """
-
+    """
+    Returns the value of a field in the Archive-It Partner API data within the metadata section.
+    If the field is repeated, returns a string with all of the values separated by semicolons.
+    If the  field is not in the data, returns the string NONE.
+    """
     try:
-        # Makes a list of all the values for that data field. Some fields may repeat, e.g. language.
         values_list = []
         for value in data['metadata'][field]:
             values_list.append(value['value'])
-
-        # Converts the list to a string.
-        # When there are multiple values in the list, each value is separated by a semicolon.
-        values = '; '.join(values_list)
+        values = ';'.join(values_list)
         return values
-
     except KeyError:
         return 'NONE'
+
+
+def save_csv_row(report_type, row_list):
+    """
+    Creates a CSV, if it doesn't already exist for this report,
+    and saves the provided list as a new row in the spreadsheet.
+    """
+    with open(f'{report_type}_metadata.csv', 'a', newline='') as output:
+        write = csv.writer(output)
+        write.writerow(row_list)
 
 
 def make_csv(json, report_type, include_optional):
