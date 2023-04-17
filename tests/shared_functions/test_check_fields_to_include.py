a45c3416a86f0538c276f8a476d3939df77004d3
Adriane Hanson <ahansonuga@gmail.com>
2023-04-17 08:41:09 -0400

    Update assert text

--- tests/shared_functions/test_check_fields_to_include.py
+++ tests/shared_functions/test_check_fields_to_include.py
@@ -14,21 +14,21 @@ class MyTestCase(unittest.TestCase):
         Tests that the function returns True when the provided argument is text other than "required".
         """
         actual = check_fields_to_include(['C:/path/seed_metadata_report.py', 'other_text'])
-        self.assertEqual(actual, True, "Problem with test argument is other text.")
+        self.assertEqual(actual, True, "Problem with test: argument is other text.")
 
     def test_arg_is_required(self):
         """
         Tests that the function returns False when the provided argument is "required".
         """
         actual = check_fields_to_include(['C:/path/seed_metadata_report.py', 'required'])
-        self.assertEqual(actual, False, "Problem with test argument is required.")
+        self.assertEqual(actual, False, "Problem with test: argument is required.")
 
     def test_no_arg(self):
         """
         Tests that the function returns True when there is no provided argument.
         """
         actual = check_fields_to_include(['C:/path/seed_metadata_report.py'])
-        self.assertEqual(actual, True, "Problem with test no argument.")
+        self.assertEqual(actual, True, "Problem with test: no argument.")
 
 
 if __name__ == '__main__':
