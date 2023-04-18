464babcdd6f88f9cd6752e71650ce9d4a439223e
Adriane M Hanson <ahansonuga@gmail.com>
2023-04-18 11:48:48 -0400

    Create test_create_new_metadata.py and test input files

--- tests/preservation_download_tracker/test_create_new_metadata.py
+++ tests/preservation_download_tracker/test_create_new_metadata.py
@@ -13,19 +13,35 @@ from preservation_download_tracker import create_new_metadata
 
 class MyTestCase(unittest.TestCase):
 
-    # TODO: make test CSVs
-    # TODO: what variations are there to test?
-
-    def test_something(self):
+    def test_multiple_warcs(self):
         """
         Tests that the function returns the expected dataframe (converted to a list for easier comparison)
-        when CONDITION.
+        when the seeds each have multiple WARCs.
         """
-        warc_df = pd.read_csv("warc_metadata_create_new_CONDITION.csv")
+        warc_df = pd.read_csv("warc_metadata_create_new_multiple_warcs.csv")
         actual_df = create_new_metadata(warc_df)
-        actual = actual_df.columns.tolist() + actual_df.values.tolist()
-        expected = []
-        self.assertEqual(actual, expected, "Problem with test for ")
+        actual = [actual_df.columns.tolist()] + [actual_df.index.tolist()] + actual_df.values.tolist()
+        expected = [["Crawl_Job_ID", "Crawl_Definition_ID", "WARC_Count", "WARC_Size_GB"],
+                    [2184360, 2547117, 2547541],
+                    ["1453015;1436127", "31104307871", 3, 2.51],
+                    ["1436594;1430000", "31104463292;31104000000", 2, 1.02],
+                    ["1436727", "31104463403", 2, 1.04]]
+        self.assertEqual(actual, expected, "Problem with test for seed with multiple WARCs")
+
+    def test_one_warc(self):
+        """
+        Tests that the function returns the expected dataframe (converted to a list for easier comparison)
+        when the seeds each have one WARC.
+        """
+        warc_df = pd.read_csv("warc_metadata_create_new_one_warc.csv")
+        actual_df = create_new_metadata(warc_df)
+        actual = [actual_df.columns.tolist()] + [actual_df.index.tolist()] + actual_df.values.tolist()
+        expected = [["Crawl_Job_ID", "Crawl_Definition_ID", "WARC_Count", "WARC_Size_GB"],
+                    [2184360, 2547117, 2547541],
+                    ["1453015", "31104307871", 1, 1],
+                    ["1436594", "31104463292", 1, .02],
+                    ["1436727", "31104463403", 1, .04]]
+        self.assertEqual(actual, expected, "Problem with test for seeds with one WARC")
 
 
 if __name__ == '__main__':
