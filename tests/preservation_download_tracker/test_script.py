cf828362369680bc24f5703f81725398e4f65e31
Adriane M Hanson <ahansonuga@gmail.com>
2023-04-19 09:27:30 -0400

    Split script tests for Hargrett and Russell

--- tests/preservation_download_tracker/test_script.py
+++ tests/preservation_download_tracker/test_script.py
@@ -21,23 +21,23 @@ class MyTestCase(unittest.TestCase):
             if file.startswith("Preservation_Download") or file.startswith("warc_metadata"):
                 os.remove(os.path.join(c.script_output, file))
 
-    def test_two(self):
+    def test_one(self):
         """
-        Tests the full script with a date range for Hargrett and Russell seeds.
+        Tests the full script with a date range for Russell seeds and one without metadata.
         Result for testing is the contents of the spreadsheet created by the script.
         """
         # Makes the WARC metadata report needed as script input.
         warc_script_path = os.path.join("..", "..", "warc_metadata_report.py")
-        subprocess.run(f"python {warc_script_path} 2021-08-20 2021-08-30", shell=True)
+        subprocess.run(f"python {warc_script_path} 2019-11-10 2019-11-25", shell=True)
 
         # Runs the script being tested.
         script_path = os.path.join("..", "..", "preservation_download_tracker.py")
-        warc_path = os.path.join(c.script_output, "warc_metadata_2021-08-20_2021-08-29.csv")
+        warc_path = os.path.join(c.script_output, "warc_metadata_2019-11-10_2019-11-24.csv")
         subprocess.run(f"python {script_path} {warc_path}", shell=True)
 
         # Reads the CSV produced by the function into a list,
         # with the contents of each row as its own list in that list.
-        report_path = os.path.join(c.script_output, "Preservation_Download_2021-08.csv")
+        report_path = os.path.join(c.script_output, "Preservation_Download_2019-11.csv")
         with open(report_path, newline="") as open_file:
             reader = csv.reader(open_file)
             actual = list(reader)
@@ -45,15 +45,41 @@ class MyTestCase(unittest.TestCase):
         expected = [["AIP_ID", "AIP_Title", "Department", "AIT_Collection_ID", "Seed_ID", "Crawl_Job_ID",
                      "Crawl_Definition_ID", "WARC_Count", "WARC_Size_GB", "Batch", "Script_Log", "Completeness_Log",
                      "QC1", "Upload", "Ingest", "QC2", "Complete"],
-                    ["", "Coronavirus (COVID-19) Information and Resources website", "Hargrett Rare Book & Manuscript Library", "12912", "2173769", "1472580;1469134", "31104307305", "2", "0.74", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Home | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547534", "1436724", "31104463400", "2", "1.48", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Posts | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547537", "1436724", "31104463400", "3", "3.4", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - About | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547535", "1436724", "31104463400", "1", "0.88", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Photos | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547539", "1436724", "31104463400", "1", "0.75", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Community | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547540", "1436724", "31104463400", "1", "0.39", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Events | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547538", "1436724", "31104463400", "1", "0.64", "", "", "", "", "", "", "", ""],
-                    ["", "Adela Yelton - Videos | Facebook", "Richard B. Russell Library for Political Research and Studies", "12265", "2547536", "1436724", "31104463400", "1", "0.26", "", "", "", "", "", "", "", ""]]
+                    ["", "Statewide Independent Living Council of Georgia", "Richard B. Russell Library for Political Research and Studies", "12264", "2070895", "1033772", "31104276639", "3", "3.64", "", "", "", "", "", "", "", ""],
+                    ["", "K7MOA Legacy Voteview Website", "Richard B. Russell Library for Political Research and Studies", "12262", "2018084", "1033408", "31104276333", "1", "0.67", "", "", "", "", "", "", "", ""],
+                    ["", "Rotary Club of Athens", "Richard B. Russell Library for Political Research and Studies", "12939", "2089429", "1025299", "31104271006", "1", "0.01", "", "", "", "", "", "", "", ""],
+                    ["", "Latest Trends - A Georgia Trend Blog", "Richard B. Russell Library for Political Research and Studies", "12939", "2091989", "1025266", "31104270979", "9", "7.49", "", "", "", "", "", "", "", ""],
+                    ["", "ACLU of Georgia", "Richard B. Russell Library for Political Research and Studies", "12944", "2090408", "1010676", "31104269622", "1", "0.52", "", "", "", "", "", "", "", ""],
+                    ["", "No title in Archive-It", "No collector in Archive-It", "12944", "2090409", "1010676", "31104269622", "1", "0.006379682", "", "", "", "", "", "", "", ""]]
 
+        self.assertEqual(actual, expected, "Problem with test one.")
+
+    def test_two(self):
+        """
+        Tests the full script with a date range for Hargrett, which are seeds with multiple Job IDs.
+        Result for testing is the contents of the spreadsheet created by the script.
+        """
+        # Makes the WARC metadata report needed as script input.
+        warc_script_path = os.path.join("..", "..", "warc_metadata_report.py")
+        subprocess.run(f"python {warc_script_path} 2020-04-16 2020-04-22", shell=True)
+
+        # Runs the script being tested.
+        script_path = os.path.join("..", "..", "preservation_download_tracker.py")
+        warc_path = os.path.join(c.script_output, "warc_metadata_2020-04-16_2020-04-21.csv")
+        subprocess.run(f"python {script_path} {warc_path}", shell=True)
+
+        # Reads the CSV produced by the function into a list,
+        # with the contents of each row as its own list in that list.
+        report_path = os.path.join(c.script_output, "Preservation_Download_2020-04.csv")
+        with open(report_path, newline="") as open_file:
+            reader = csv.reader(open_file)
+            actual = list(reader)
+
+        expected = [["AIP_ID", "AIP_Title", "Department", "AIT_Collection_ID", "Seed_ID", "Crawl_Job_ID",
+                     "Crawl_Definition_ID", "WARC_Count", "WARC_Size_GB", "Batch", "Script_Log", "Completeness_Log",
+                     "QC1", "Upload", "Ingest", "QC2", "Complete"],
+                    ["", "UGA Today", "Hargrett Rare Book & Manuscript Library", "12912", "2184360", "1130531;1130526", "31104307871;31104312482", "31", "32.878337596", "", "", "", "", "", "", "", ""],
+                    ["", "Coronavirus (COVID-19) Information and Resources website", "Hargrett Rare Book & Manuscript Library", "12912", "2173769", "1130525;1130530", "31104312481;31104307305", "2", "0.004255981000000001", "", "", "", "", "", "", "", ""]]
         self.assertEqual(actual, expected, "Problem with test two.")
 
     def test_three(self):
